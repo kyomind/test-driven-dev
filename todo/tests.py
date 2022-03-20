83c1bfb25d0282032ca476b62ee0834e69a4bb9c
kyo <odinxp@gmail.com>
2022-03-21 02:24:40 +0800

    feat: add home_page view

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+
+from todo.views import home_page
 
 # Create your tests here.
+
+
+class HomePageTest(TestCase):
+    def test_home_page_url_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
